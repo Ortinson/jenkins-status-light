ffafadfd225957a93fb384d37d84f33553ea0828
Enrique <eopqng@gmail.com>
2018-11-04 12:19:24 +0100

    change web server library to use SPIFFS

--- src/application/configuration_server.cpp
+++ src/application/configuration_server.cpp
@@ -4,41 +4,4 @@
 ConfigurationServer::ConfigurationServer(ConfigurationStorage* storage)
   : _storage(storage) {
     this->_config = this->_storage->GetStoredConfig();
-    StartServer();
   }
-
-  void ConfigurationServer::handleClient() {
-    this->_server->handleClient();
-  }
-
-  void ConfigurationServer::StartServer() {
-    this->_server = new ESP8266WebServer(80);
-    this->_server->on(String(F("/")), std::bind(&ConfigurationServer::handleRoot, this));
-    this->_server->begin();
-  }
-
-  void ConfigurationServer::handleRoot() {
-    const char* _customHeadElement = "";
-    const char* _apName = "no-net";
-    String page = FPSTR(_HTTP_HEAD);
-    page.replace("{v}", "Options");
-    page += FPSTR(_HTTP_SCRIPT);
-    page += FPSTR(_HTTP_STYLE);
-    page += _customHeadElement;
-    page += FPSTR(_HTTP_HEAD_END);
-    page += String(F("<h1>"));
-    page += _apName;
-    page += String(F("</h1>"));
-    page += String(F("<h3>WiFiManager</h3>"));
-    page += FPSTR(_HTTP_PORTAL_OPTIONS);
-    page += FPSTR(_HTTP_END);
-
-    this->_server->sendHeader("Content-Length", String(page.length()));
-    this->_server->send(200, "text/html", page);
-
-  }
-
-//Initiate server
-// store new config when requested by website
-  // find example where each page can be one file
-  // add one example of changing one parameter from root page
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,10 @@
 #include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
 
-//needed for library
-#include <DNSServer.h>
-#include <ESP8266WebServer.h>
-#include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
+// Needed for wifi manager library
+// #include <DNSServer.h>
+// #include <ESP8266WebServer.h>
+// #include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
+
 #include "application/configuration_storage.h"
 #include "application/configuration_server.h"
 
@@ -11,18 +12,18 @@ ConfigurationStorage* storage;
 ConfigurationServer* server;
 void setup() {
     Serial.begin(115200);
-    delay(3000);
     Serial.println("starting setup");
     storage = new ConfigurationStorage();
     server = new ConfigurationServer(storage);
-    // Initialize ConfigurationServer
-    // Initialize LEDDriver
-    // Initialize JenkinsMonitor
-    // Setup led driver and set it to notify connection mode
+    // TODO(Ortinson): Initialize LEDDriver
+    // TODO(Ortinson): Initialize JenkinsMonitor
+    // TODO(Ortinson): Setup led driver and set it to notify connection mode
 
-    WiFiManager wifiManager;
-    wifiManager.autoConnect(storage->GetStoredConfig().device_name);
-    Serial.println("connected...yeey :)");
+
+    // TODO(Ortinson): find a way to manage wifi connection that is compatible
+    //   with 'ESP async web server'    // WiFiManager wifiManager;
+    // wifiManager.autoConnect(storage->GetStoredConfig().device_name);
+    // Serial.println("connected...yeey :)");
 
     // Start jenkins monitor
 }
@@ -31,7 +32,6 @@ void loop() {
 
   while(true){
     Serial.println("caca");
-    server->handleClient();
     delay(1000);
   }
     // put your main code here, to run repeatedly:
